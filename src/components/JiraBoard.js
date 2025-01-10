59f68445e66345c90a37dbf582448d75e62f24c2
Harry Cooper <harry@bigbite.net>
2025-01-10 23:31:20 +0000

    feat: add assignee selector

--- src/components/JiraBoard.js
+++ src/components/JiraBoard.js
@@ -21,6 +21,7 @@ const JiraBoard = ( {
 	const [currentSprint, setCurrentSprint] = useState(null);
 	const [tickets, setTickets] = useState([]);
 	const [assignee, setAssignee] = useState('all');
+	const [assigneeOptions, setAssigneeOptions] = useState([]);
 
 	/**
 	 * Fetch the boards columns
@@ -103,6 +104,41 @@ const JiraBoard = ( {
 
 		// set the tickets
 		setTickets(ticketsJSON.issues);
+
+		// set the assignee options
+		let tempAssigneeOptions = ticketsJSON.issues.reduce((acc, ticket) => {
+			if ( ! ticket.fields.assignee ) {
+				acc['unassigned'] = 'Unassigned';
+			} else {
+				acc[ticket.fields.assignee.emailAddress] = ticket.fields.assignee.displayName;
+			}
+
+			return acc;
+		}, {});
+
+		// if unassigned is there, move it to the front
+		if ( tempAssigneeOptions.unassigned ) {
+			tempAssigneeOptions = {
+				unassigned: tempAssigneeOptions.unassigned,
+				...tempAssigneeOptions
+			};
+		}
+
+		// if the current user is there, change the value to 'Current User' and move it to the front
+		if ( tempAssigneeOptions[board.info.email] ) {
+			tempAssigneeOptions[board.info.email] = 'Current User';
+			tempAssigneeOptions = {
+				[board.info.email]: 'Current User',
+				...tempAssigneeOptions
+			};
+		}
+
+		tempAssigneeOptions = {
+			all: 'All',
+			...tempAssigneeOptions
+		};
+
+		setAssigneeOptions(tempAssigneeOptions);
 	};
 
 	useEffect(() => {
@@ -112,13 +148,36 @@ const JiraBoard = ( {
 
 	return (
 		<div className="jira-board">
+			<div className="jira-board-header">
+				<p className="jira-board-sprint">
+					<strong>Sprint:</strong> {currentSprint?.name}
+					<span
+						className={`jira-sprint-indicator ${currentSprint?.state}`}
+						title={`Sprint ${currentSprint?.state}`}
+					>
+						⦿
+					</span>
+				</p>
+				<div className="jira-board-assignee">
+					<label htmlFor="jira-assignee"><strong>Assignee:</strong></label>
+					<select
+						id="jira-assignee"
+						value={assignee}
+						onChange={e => setAssignee(e.target.value)}
+					>
+						{Object.keys(assigneeOptions).map( key => (
+							<option key={key} value={key}>{assigneeOptions[key]}</option>
+						))}
+					</select>
+				</div>
+			</div>
 			<ul className="jira-columns" style={{ gridTemplateColumns: `repeat(${columns.length}, 19%)` }}>
 				{columns.map((column, index) => (
 					<li key={index} className="jira-column">
 						<h4>{column.name}</h4>
 						<ul className="jira-tickets">
 							{tickets
-								.filter( ticket => assignee === 'all' || ticket.fields.assignee?.emailAddress === board.info.email )
+								.filter( ticket => assignee === 'all' || ticket.fields.assignee?.emailAddress === assignee || ( assignee === 'unassigned' && ! ticket.fields.assignee ) )
 								.filter( ticket => column.statuses.some( status => status.id === ticket.fields.status.id ) )
 								.map( (ticket, i) => (
 									<JiraTicket key={i} ticket={ticket} />
--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -21,7 +21,7 @@ export const HidePasswordIcon = () => (
 
 export const UnassignedIcon = () => (
 	<svg width="2rem" height="2rem" viewBox="0 0 24 24" role="presentation">
-		<g fill="currentcolor" fill-rule="evenodd">
+		<g fill="currentcolor" fillRule="evenodd">
 			<path d="M6 14c0-1.105.902-2 2.009-2h7.982c1.11 0 2.009.894 2.009 2.006v4.44c0 3.405-12 3.405-12 0z"></path>
 			<circle cx="12" cy="7" r="4"></circle>
 		</g>
